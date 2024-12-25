41440996af8372e2ad77d75d177192f3b51aa009
shayg <shaygali10@gmail.com>
2024-12-25 20:01:31 +0200

    register work

--- app/src/main/java/com/sibi/helpi/LoginFragment.java
+++ app/src/main/java/com/sibi/helpi/LoginFragment.java
@@ -11,23 +11,29 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginFragment extends Fragment {
+    private FirebaseAuth mAuth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
-    }
+        View inflate = inflater.inflate(R.layout.fragment_login, container, false);
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+//            Navigation.findNavController(inflate).navigate(R.id.action_login_fragment_to_homeFragment);
+            Navigation.findNavController(requireActivity(), R.id.nav_host_fragment).navigate(R.id.action_login_fragment_to_homeFragment);
+        }
 
         //TODO: check if the user in sgin in or not (https://firebase.google.com/docs/auth/android/start?hl=he#java)
 
-        Button loginButton = view.findViewById(R.id.login_button);
-        Button goToRegisterButton = view.findViewById(R.id.got_to_reg_button);
+        Button loginButton = inflate.findViewById(R.id.login_button);
+        Button goToRegisterButton = inflate.findViewById(R.id.got_to_reg_button);
 
 
         loginButton.setOnClickListener(v ->
@@ -38,9 +44,16 @@ public class LoginFragment extends Fragment {
                 Navigation.findNavController(v).navigate(R.id.action_login_fragment_to_registerFragment)
         );
 
-        View facebook_button = view.findViewById(R.id.facebook_button);
+        View facebook_button = inflate.findViewById(R.id.facebook_button);
         facebook_button.setOnClickListener(v ->
                 Navigation.findNavController(v).navigate(R.id.action_login_fragment_to_testFragment)
         );
+
+        View Google_button = inflate.findViewById(R.id.google_button);
+        Google_button.setOnClickListener(v ->
+                Navigation.findNavController(v).navigate(R.id.action_login_fragment_to_advancedSearchFragment)
+        );
+        return inflate;
     }
+
 }
--- app/src/main/java/com/sibi/helpi/RegisterFragment.java
+++ app/src/main/java/com/sibi/helpi/RegisterFragment.java
@@ -1,16 +1,29 @@
 package com.sibi.helpi;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.concurrent.Executor;
 
 public class RegisterFragment extends Fragment {
+    private FirebaseAuth mAuth;
 
     public RegisterFragment() {
         // Required empty public constructor
@@ -19,7 +32,6 @@ public class RegisterFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -27,6 +39,9 @@ public class RegisterFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View inflate = inflater.inflate(R.layout.fragment_register, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+
         EditText fNameEditText = inflate.findViewById(R.id.first_name_input_reg);
         EditText lNameEditText = inflate.findViewById(R.id.last_name_input_reg);
         EditText emailEditText = inflate.findViewById(R.id.email);
@@ -65,9 +80,21 @@ public class RegisterFragment extends Fragment {
 
             if (firstToFocus != null) {
                 firstToFocus.requestFocus();
+                return;
             }
 
-
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getContext(), "User created successfully", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            // TODO: show the error message to the user in a better way
+                            Toast.makeText(getContext(), "Authentication failed." + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
         });
 
         return inflate;
