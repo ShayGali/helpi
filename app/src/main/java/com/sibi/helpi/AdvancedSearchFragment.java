3921a4b548522e91307d54ee4cdc3b9a6197541b
shayg <shaygali10@gmail.com>
2024-12-26 19:10:09 +0200

    navigation work

--- app/src/main/java/com/sibi/helpi/AdvancedSearchFragment.java
+++ app/src/main/java/com/sibi/helpi/AdvancedSearchFragment.java
@@ -5,11 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Toast;
+import android.widget.Spinner;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.textfield.MaterialAutoCompleteTextView;
 
 public class AdvancedSearchFragment extends Fragment {
@@ -23,12 +24,19 @@ public class AdvancedSearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_advanced_search, container, false);
-        MaterialAutoCompleteTextView categorySpinner = view.findViewById(R.id.category_spinner);
+
+        Spinner categorySpinner = view.findViewById(R.id.spinnerCategories);
         String[] categories = {"Category 1", "Category 2", "Category 3", "Category 4"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_dropdown_item_1line, categories);
         categorySpinner.setAdapter(adapter);
 
-        categorySpinner.setOnClickListener(v -> categorySpinner.showDropDown());
+        Spinner subCategorySpinner = view.findViewById(R.id.spinnerSubCategories);
+        String[] subCategories = {"SubCategory 1", "SubCategory 2", "SubCategory 3", "SubCategory 4"};
+        ArrayAdapter<String> subAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_dropdown_item_1line, subCategories);
+        subCategorySpinner.setAdapter(subAdapter);
+
+
+//        categorySpinner.setOnClickListener(v -> categorySpinner.performClick());
         return view;
 
     }
