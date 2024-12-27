53c558fe30e5a9a595bebc0024f1ff64a0aa87b2
ShayGali <shaygali100@gmail.com>
2024-12-27 11:19:37 +0200

    add offer page layout, and bg color to app

--- app/src/main/java/com/sibi/helpi/OfferProductFragment.java
+++ app/src/main/java/com/sibi/helpi/OfferProductFragment.java
@@ -7,58 +7,53 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OfferProductFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class OfferProductFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public OfferProductFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OfferProductFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static OfferProductFragment newInstance(String param1, String param2) {
-        OfferProductFragment fragment = new OfferProductFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    Spinner categorySpinner;
+    Spinner subcategorySpinner;
+    Spinner regionSpinner;
+    Spinner productStatusSpinner;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_offer_product, container, false);
+        View view = inflater.inflate(R.layout.fragment_offer_product, container, false);
+
+        categorySpinner = view.findViewById(R.id.spinnerCategories);
+        subcategorySpinner = view.findViewById(R.id.spinnerSubCategory);
+        regionSpinner = view.findViewById(R.id.spinnerRegion);
+        productStatusSpinner = view.findViewById(R.id.spinnerProductSituation);
+
+        String[] categories = getResources().getStringArray(R.array.categories);
+        //TODO: get the subcategories based on the selected category
+        String[] electronicsSubcategories = getResources().getStringArray(R.array.electronics_subcategories);
+        String[] regions = getResources().getStringArray(R.array.region);
+        String[] productStatus = getResources().getStringArray(R.array.product_status);
+
+        ArrayAdapter<String> categoryAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_dropdown_item_1line, categories);
+        categorySpinner.setAdapter(categoryAdapter);
+
+        ArrayAdapter<String> subAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_dropdown_item_1line, electronicsSubcategories);
+        subcategorySpinner.setAdapter(subAdapter);
+
+        ArrayAdapter<String> regionAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_dropdown_item_1line, regions);
+        regionSpinner.setAdapter(regionAdapter);
+
+        ArrayAdapter<String> productStatusAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_dropdown_item_1line, productStatus);
+        productStatusSpinner.setAdapter(productStatusAdapter);
+
+
+        return view;
     }
 }
