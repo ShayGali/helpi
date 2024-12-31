4dc4bd2552bf0648fa933a2cbca34980eaa1dbfc
shayg <shaygali10@gmail.com>
2024-12-31 11:45:52 +0200

    app google maps api key

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+val secretsFile = file("secrets.properties")
+extra.apply {
+    if (secretsFile.exists()) {
+        val properties = java.util.Properties()
+        properties.load(secretsFile.inputStream())
+        properties.forEach { (key, value) ->
+            set(key.toString(), value)
+        }
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
