defa42578392e04a02dee1eab73b9c4b0df6c42f
ShayGali <shaygali100@gmail.com>
2025-01-19 10:37:37 +0200

    update

--- app/src/main/java/com/sibi/helpi/fragments/SearchProductFragment.java
+++ app/src/main/java/com/sibi/helpi/fragments/SearchProductFragment.java
@@ -3,15 +3,12 @@ package com.sibi.helpi.fragments;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.InputType;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.core.content.ContextCompat;
@@ -25,6 +22,7 @@ public class SearchProductFragment extends Fragment {
 
     private AutoCompleteTextView categorySpinner, subcategorySpinner, regionSpinner, productStatusSpinner;
     private TextInputLayout categoryInputLayout, subcategoryInputLayout, regionInputLayout, productStatusInputLayout;
+    private Button submitSearchButton;
 
     public SearchProductFragment() {
         // Required empty public constructor
@@ -39,7 +37,7 @@ public class SearchProductFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_search_product, container, false);
-        Button submitSearchButton = view.findViewById(R.id.btnSearchProduct);
+        submitSearchButton = view.findViewById(R.id.btnSearchProduct);
 
         categorySpinner = view.findViewById(R.id.categorySpinner);
         subcategorySpinner = view.findViewById(R.id.subcategorySpinner);
@@ -56,42 +54,27 @@ public class SearchProductFragment extends Fragment {
         String[] regions = getResources().getStringArray(R.array.region);
         String[] productStatus = getResources().getStringArray(R.array.product_status);
 
-
         // Setup the AutoCompleteTextViews with background, rounded corners, and adapter
         setupAutoCompleteTextView(categorySpinner, categoryInputLayout, categories);
         setupAutoCompleteTextView(regionSpinner, regionInputLayout, regions);
         setupAutoCompleteTextView(productStatusSpinner, productStatusInputLayout, productStatus);
 
-//
-//        submitSearchButton.setOnClickListener(v -> {
-//            Bundle bundle = new Bundle();
-//            if (categorySpinner.getSelectedItem().toString().equals(getResources().getStringArray(R.array.categories)[0])) {
-//                bundle.putString("category", "");
-//            } else {
-//                bundle.putString("category", categorySpinner.getSelectedItem().toString());
-//            }
-//
-//
-//            if (subcategorySpinner.getSelectedItem() == null || subcategorySpinner.getSelectedItem().toString().equals(getResources().getStringArray(R.array.electronics_subcategories)[0])) {
-//                bundle.putString("subcategory", "");
-//            } else {
-//                bundle.putString("subcategory", subcategorySpinner.getSelectedItem().toString());
-//            }
-//
-//            if (regionSpinner.getSelectedItem().toString().equals(getResources().getStringArray(R.array.region)[0])) {
-//                bundle.putString("region", "");
-//            } else {
-//                bundle.putString("region", regionSpinner.getSelectedItem().toString());
-//            }
-//
-//            if (productStatusSpinner.getSelectedItem().toString().equals(getResources().getStringArray(R.array.product_status)[0])) {
-//                bundle.putString("productStatus", "");
-//            } else {
-//                bundle.putString("productStatus", productStatusSpinner.getSelectedItem().toString());
-//            }
-//            // navigate to the search result fragment and add the bundle
-//            Navigation.findNavController(view).navigate(R.id.action_searchProductFragment_to_searchProductResultFragment, bundle);
-//        });
+        // Disable subcategory spinner initially
+        setSpinnerEnabled(subcategorySpinner, false);
+
+        // Set up the subcategory spinner based on the selected category
+        setupCategorySpinner();
+
+        submitSearchButton.setOnClickListener(v -> {
+            Bundle bundle = new Bundle();
+            bundle.putString("category", categorySpinner.getText().toString());
+            bundle.putString("subcategory", subcategorySpinner.getText().toString());
+            bundle.putString("region", regionSpinner.getText().toString());
+            bundle.putString("productStatus", productStatusSpinner.getText().toString());
+
+            // navigate to the search result fragment and add the bundle
+            Navigation.findNavController(view).navigate(R.id.action_searchProductFragment_to_searchProductResultFragment, bundle);
+        });
 
         return view;
     }
@@ -151,75 +134,48 @@ public class SearchProductFragment extends Fragment {
             }
         });
     }
-//
-//    private void setupSpinners() {
-//        // Set up the spinners with default values
-//        setUpSpinner(categorySpinner, R.array.categories);
-//        setUpSpinner(regionSpinner, R.array.region);
-//        setUpSpinner(productStatusSpinner, R.array.product_status);
-//
-//        // make the subcategorySpinner unclickable until a category is selected
-//        subcategorySpinner.setEnabled(false);
-//        categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//                subcategorySpinner.setEnabled(position != 0);
-//                if (position != 0) {
-//                    subcategorySpinner.setEnabled(true);
-//                    switch (position) {
-//                        case 1:
-//                            setUpSpinner(subcategorySpinner, R.array.electronics_subcategories);
-//                            break;
-//                        case 2:
-//                            setUpSpinner(subcategorySpinner, R.array.fashion_subcategories);
-//                            break;
-//                        case 3:
-//                            setUpSpinner(subcategorySpinner, R.array.books_subcategories);
-//                            break;
-//                        case 4:
-//                            setUpSpinner(subcategorySpinner, R.array.home_subcategories);
-//                            break;
-//                        case 5:
-//                            setUpSpinner(subcategorySpinner, R.array.toys_subcategories);
-//                        case 6:
-//                            setUpSpinner(subcategorySpinner, R.array.other_subcategories);
-//                            subcategorySpinner.setEnabled(false);
-//                            break;
-//                    }
-//                }
-//            }
-//
-//            @Override
-//            public void onNothingSelected(AdapterView<?> parent) {
-//                subcategorySpinner.setEnabled(false);
-//            }
-//        });
-//    }
 
-    private void setUpSpinner(Spinner spinner, int arrayResId) {
-        String[] items = getResources().getStringArray(arrayResId);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), R.layout.spinner_item, items) {
-            @Override
-            public boolean isEnabled(int position) {
-                return position != 0; // Disable the default text
+    private void setupCategorySpinner() {
+        categorySpinner.setOnItemClickListener((parent, view, position, id) -> {
+            String selectedCategory = categorySpinner.getText().toString();
+            int subcategoryArrayId = getSubcategoryArrayId(selectedCategory);
+            if (subcategoryArrayId != 0) {
+                String[] subcategories = getResources().getStringArray(subcategoryArrayId);
+                setupAutoCompleteTextView(subcategorySpinner, subcategoryInputLayout, subcategories);
+                setSpinnerEnabled(subcategorySpinner, true);
+            } else {
+                setSpinnerEnabled(subcategorySpinner, false);
             }
+        });
+    }
 
-            @Override
-            public View getDropDownView(int position, View convertView, ViewGroup parent) {
-                View view = super.getDropDownView(position, convertView, parent);
-                TextView textView = (TextView) view;
-                if (position == 0) {
-                    textView.setTextColor(Color.GRAY); // Set the default text color to gray
-                } else {
-                    textView.setTextColor(Color.BLACK); // Set the other items text color to black
-                }
-                return view;
-            }
-        };
+    private int getSubcategoryArrayId(String category) {
+        switch (category) {
+            case "Electronics":
+                return R.array.electronics_subcategories;
+            case "Fashion":
+                return R.array.fashion_subcategories;
+            case "Books":
+                return R.array.books_subcategories;
+            case "Home":
+                return R.array.home_subcategories;
+            case "Toys":
+                return R.array.toys_subcategories;
+            case "Other":
+                return R.array.other_subcategories;
+            default:
+                return 0;
+        }
+    }
 
-        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setSelection(0); // Set the default value
+    private void setSpinnerEnabled(AutoCompleteTextView spinner, boolean enabled) {
+        spinner.setEnabled(enabled);
+        if (enabled) {
+            spinner.setBackgroundResource(R.drawable.spinner_background);
+            spinner.setTextColor(ContextCompat.getColor(requireContext(), R.color.black));
+        } else {
+            spinner.setBackgroundResource(R.drawable.spinner_disabled_background);
+            spinner.setTextColor(ContextCompat.getColor(requireContext(), R.color.gray));
+        }
     }
 }
