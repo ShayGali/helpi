5eb8ceeb062dceef06a38e2d744732c7fb42b94b
ShayGali <shaygali100@gmail.com>
2025-02-09 21:41:24 +0200

    make the search of post when click go to the right screen

--- app/src/main/java/com/sibi/helpi/fragments/PostablePageFragment.java
+++ app/src/main/java/com/sibi/helpi/fragments/PostablePageFragment.java
@@ -1,54 +1,3 @@
-//package com.sibi.helpi;
-//
-//import android.os.Bundle;
-//
-//import androidx.fragment.app.Fragment;
-//import androidx.viewpager2.widget.ViewPager2;
-//
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//
-//import com.sibi.helpi.adapters.ImageSliderAdapter;
-//
-//
-//public class ProductPageFragment extends Fragment {
-//
-//
-//    public ProductPageFragment() {
-//        // Required empty public constructor
-//    }
-//
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//    }
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        View view = inflater.inflate(R.layout.fragment_product_page, container, false);
-//        int[] imageResIds = new int[]{
-//                R.drawable.red_balloon,
-//                R.drawable.blue_balon
-//        };
-//
-//        // Convert resource IDs to URI strings
-//        String[] imageUrls = new String[imageResIds.length];
-//        for (int i = 0; i < imageResIds.length; i++) {
-//            imageUrls[i] = "android.resource://" + getContext().getPackageName() + "/" + imageResIds[i];
-//        }
-//
-//        ImageSliderAdapter adapter = new ImageSliderAdapter(getContext(), imageUrls);
-//
-//        ViewPager2 viewPager = view.findViewById(R.id.imageSlider);
-//        viewPager.setAdapter(adapter);
-//        return view;
-//    }
-//}
-
 package com.sibi.helpi.fragments;
 
 import android.os.Bundle;
@@ -63,54 +12,38 @@ import android.widget.TextView;
 
 import com.sibi.helpi.R;
 import com.sibi.helpi.adapters.ImageSliderAdapter;
+import com.sibi.helpi.models.Postable;
 
 public class PostablePageFragment extends Fragment {
 
-    private
-    ViewPager2 productImages;
+    private ViewPager2 productImages;
     private TextView productTitle;
-    private TextView productCategory;
-    private TextView productSubCategory;
-    private TextView productRegion;
-    private TextView productStatus;
+    private TextView productDescription;
+    private TextView deliveryPersonName;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_postable_page, container, false);
 
         productImages = view.findViewById(R.id.imageSlider);
+        productTitle = view.findViewById(R.id.postableTitle);
+        productDescription = view.findViewById(R.id.postDescription);
+        deliveryPersonName = view.findViewById(R.id.deliveryPersonName);
 
-//        productTitle = view.findViewById(R.id.productTitle);
-//        productCategory = view.findViewById(R.id.productCategory);
-//        productSubCategory = view.findViewById(R.id.productSubCategory);
-//        productRegion = view.findViewById(R.id.productRegion);
-//        productStatus = view.findViewById(R.id.productStatus);
-        TextView productDescription = view.findViewById(R.id.productDescription);
+        if (getArguments() != null) {
+            Postable postable = (Postable) getArguments().getSerializable("postable");
 
-//        if (getArguments() != null) {
-//            productTitle.setText(getArguments().getString("productTitle"));
-//            productCategory.setText(getArguments().getString("productCategory"));
-//            productSubCategory.setText(getArguments().getString("productSubCategory"));
-//            productRegion.setText(getArguments().getString("productRegion"));
-//            productStatus.setText(getArguments().getString("productStatus"));
-//            productImage.setImageResource(getArguments().getInt("productImageResourceId"));
-//        }
-//        ImageSliderAdapter adapter = new ImageSliderAdapter(getContext(), imageUrls);
+            if (postable != null) {
+                productTitle.setText(postable.getTitle());
+                productDescription.setText(postable.getDescription());
+//                deliveryPersonName.setText(postable.getDeliveryPersonName()); //TODO - fetch delivery person data from the repository
 
-        int[] imageResIds = new int[]{
-                R.drawable.red_balloon,
-                R.drawable.blue_balon
-        };
-
-        // Convert resource IDs to URI strings
-        String[] imageUrls = new String[imageResIds.length];
-        for (int i = 0; i < imageResIds.length; i++) {
-            imageUrls[i] = "android.resource://" + getContext().getPackageName() + "/" + imageResIds[i];
+                // Assuming Postable has a method getImageUrls() that returns a list of image URLs
+                String[] imageUrls = postable.getImageUrls().toArray(new String[0]);
+                ImageSliderAdapter adapter = new ImageSliderAdapter(getContext(), imageUrls);
+                productImages.setAdapter(adapter);
+            }
         }
-
-        ImageSliderAdapter adapter = new ImageSliderAdapter(getContext(), imageUrls);
-
-        productImages.setAdapter(adapter);
 
         return view;
     }
--- app/src/main/java/com/sibi/helpi/models/Postable.java
+++ app/src/main/java/com/sibi/helpi/models/Postable.java
@@ -2,21 +2,34 @@ package com.sibi.helpi.models;
 
 import com.sibi.helpi.utils.AppConstants.PostStatus;
 
+import java.io.Serializable;
 import java.util.List;
 
-public interface Postable {
+public interface Postable extends Serializable {
     String getId();
+
     void setId(String id);
+
     String getTitle();
+
     String getDescription();
+
     String getCategory();
+
     String getSubCategory();
+
     String getRegion();
+
     String getUserId();
+
     List<String> getImageUrls();
+
     void setImageUrls(List<String> imageUrls);
+
     PostStatus getStatus();
+
     void setStatus(PostStatus status);
+
     String toString();
 
 }
