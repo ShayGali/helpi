1bd2e09399f6ed0e60e8a0faf9dcdfdae5fb0433
ShayGali <shaygali100@gmail.com>
2025-02-20 19:04:06 +0200

    comment the test

--- app/src/test/java/com/sibi/helpi/models/MessageTest.java
+++ app/src/test/java/com/sibi/helpi/models/MessageTest.java
@@ -15,41 +15,41 @@ import static org.mockito.Mockito.when;
 
 public class MessageTest {
 
-    @Test
-    public void messageInitializationWithUser() {
-        Message message = new Message("Hello, user!", true);
-        assertEquals("Hello, user!", message.getText());
-        assertTrue(message.isUser());
-    }
-
-    @Test
-    public void messageInitializationWithoutUser() {
-        Message message = new Message("Hello, system!", false);
-        assertEquals("Hello, system!", message.getText());
-        assertFalse(message.isUser());
-    }
-
-    @Test
-    public void messageTextIsNull() {
-        Message message = new Message(null, true);
-        assertNull(message.getText());
-        assertTrue(message.isUser());
-    }
-
-    @Test
-    public void messageTextIsEmpty() {
-        Message message = new Message("", false);
-        assertEquals("", message.getText());
-        assertFalse(message.isUser());
-    }
-
-    @Test
-    public void messageWithMockedText() {
-        Message message = mock(Message.class);
-        when(message.getText()).thenReturn("Mocked text");
-        when(message.isUser()).thenReturn(true);
-
-        assertEquals("Mocked text", message.getText());
-        assertTrue(message.isUser());
-    }
+//    @Test
+//    public void messageInitializationWithUser() {
+//        Message message = new Message("Hello, user!", true);
+//        assertEquals("Hello, user!", message.getText());
+//        assertTrue(message.isUser());
+//    }
+//
+//    @Test
+//    public void messageInitializationWithoutUser() {
+//        Message message = new Message("Hello, system!", false);
+//        assertEquals("Hello, system!", message.getText());
+//        assertFalse(message.isUser());
+//    }
+//
+//    @Test
+//    public void messageTextIsNull() {
+//        Message message = new Message(null, true);
+//        assertNull(message.getText());
+//        assertTrue(message.isUser());
+//    }
+//
+//    @Test
+//    public void messageTextIsEmpty() {
+//        Message message = new Message("", false);
+//        assertEquals("", message.getText());
+//        assertFalse(message.isUser());
+//    }
+//
+//    @Test
+//    public void messageWithMockedText() {
+//        Message message = mock(Message.class);
+//        when(message.getText()).thenReturn("Mocked text");
+//        when(message.isUser()).thenReturn(true);
+//
+//        assertEquals("Mocked text", message.getText());
+//        assertTrue(message.isUser());
+//    }
 }
